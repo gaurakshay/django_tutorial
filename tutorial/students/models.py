971c3865249316698d16060685df22fd66921975
gaurakshay <gaur.akshay@gmail.com>
2018-06-05 09:03:36 -0500

    Registered the app and generated table for model in db

--- tutorial/students/models.py
+++ tutorial/students/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 
-Class Department(models.Model):
+class Department(models.Model):
     """
     This model will store the details about our department.
     Primary key for this model is department code.
@@ -25,44 +25,44 @@ Class Department(models.Model):
         return self.d_name
 
 
-Class Courses(models.Model):
-    """
-    This model will store the details about courses
-    present in the sytem.
-    Primary key for this model is a combination of
-    course code and department code (compound key)
-    """
-    class Meta:
-        db_table = 'courses'
-        unique_together = (('c_d_code', 'c_code'), )
-
-    c_d_code = models.ForeignKey(Department)
-    c_code = models.IntegerField()
-    c_name = models.CharField(max_length=200)
-    c_seats = models.IntegerField()
-    c_desc = models.TextField(blank=True)
-
-    def __unicode__(self):
-        """
-        String representation of the object.
-        """
-        return self.c_name
-
-
-Class Students(models.Model):
-    """
-    This model will store our student's details.
-    Primary key will be the student's id.
-    """
-
-    class Meta:
-        db_table = 'students'
-        ordering = ['student_id']
-
-    s_id = IntegerField(primary_key=True)
-    s_first_name = CharField(max_length=200)
-    s_last_name = CharField(max_length=200)
-    s_pic = ImageField(upload_to='student_pics', blank=True)
-    courses = ManyToManyField(Course)
+#class Courses(models.Model):
+#    """
+#    This model will store the details about courses
+#    present in the sytem.
+#    Primary key for this model is a combination of
+#    course code and department code (compound key)
+#    """
+#    class Meta:
+#        db_table = 'courses'
+#        unique_together = (('c_d_code', 'c_code'), )
+#
+#    c_d_code = models.ForeignKey(Department)
+#    c_code = models.IntegerField()
+#    c_name = models.CharField(max_length=200)
+#    c_seats = models.IntegerField()
+#    c_desc = models.TextField(blank=True)
+#
+#    def __unicode__(self):
+#        """
+#        String representation of the object.
+#        """
+#        return self.c_name
+#
+#
+#class Students(models.Model):
+#    """
+#    This model will store our student's details.
+#    Primary key will be the student's id.
+#    """
+#
+#    class Meta:
+#        db_table = 'students'
+#        ordering = ['student_id']
+#
+#    s_id = IntegerField(primary_key=True)
+#    s_first_name = CharField(max_length=200)
+#    s_last_name = CharField(max_length=200)
+#    s_pic = ImageField(upload_to='student_pics', blank=True)
+#    courses = ManyToManyField(Course)
 
 
--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'students',
 ]
 
 MIDDLEWARE = [
@@ -73,10 +74,20 @@ WSGI_APPLICATION = 'tutorial.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+#DATABASES = {
+    #'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tutorial_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
