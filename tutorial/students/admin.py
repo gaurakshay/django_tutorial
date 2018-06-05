a369da8d1f79f14b57eadba2b98398eec051014c
gaurakshay <gaur.akshay@gmail.com>
2018-06-05 14:16:36 -0500

    added all the models and completed migration

--- tutorial/students/admin.py
+++ tutorial/students/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+
+from students.models import Department, Course, Student
+
 # Register your models here.
+admin.site.register(Department)
+admin.site.register(Course)
+admin.site.register(Student)
--- tutorial/students/models.py
+++ tutorial/students/models.py
@@ -12,11 +12,11 @@ class Department(models.Model):
         db_table = "departments"
         ordering = ['d_name']
 
-    d_name = models.CharField(max_length=200)
-    d_code = models.CharField(max_length=5, primary_key=True)
-    d_chair = models.CharField(max_length=200)
+    d_name = models.CharField(max_length=200, verbose_name="Department Name")
+    d_code = models.CharField(max_length=5, primary_key=True, verbose_name="Department Code")
+    d_chair = models.CharField(max_length=200, verbose_name="Department Chair")
 
-    def __unicode__(self):
+    def __str__(self):
         """
         String representation of the department.
         This will be used mainly when we print
@@ -25,44 +25,53 @@ class Department(models.Model):
         return self.d_name
 
 
-#class Courses(models.Model):
-#    """
-#    This model will store the details about courses
-#    present in the sytem.
-#    Primary key for this model is a combination of
-#    course code and department code (compound key)
-#    """
-#    class Meta:
-#        db_table = 'courses'
-#        unique_together = (('c_d_code', 'c_code'), )
-#
-#    c_d_code = models.ForeignKey(Department)
-#    c_code = models.IntegerField()
-#    c_name = models.CharField(max_length=200)
-#    c_seats = models.IntegerField()
-#    c_desc = models.TextField(blank=True)
-#
-#    def __unicode__(self):
-#        """
-#        String representation of the object.
-#        """
-#        return self.c_name
-#
-#
-#class Students(models.Model):
-#    """
-#    This model will store our student's details.
-#    Primary key will be the student's id.
-#    """
-#
-#    class Meta:
-#        db_table = 'students'
-#        ordering = ['student_id']
-#
-#    s_id = IntegerField(primary_key=True)
-#    s_first_name = CharField(max_length=200)
-#    s_last_name = CharField(max_length=200)
-#    s_pic = ImageField(upload_to='student_pics', blank=True)
-#    courses = ManyToManyField(Course)
+class Course(models.Model):
+    """
+    This model will store the details about courses
+    present in the sytem.
+    Primary key for this model is a combination of
+    course code and department code (compound key)
+    """
+    class Meta:
+        """
+        Compound key is defined by the keyword unique_together
+        """
+        db_table = 'courses'
+        unique_together = (('department', 'c_code'), )
+
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, verbose_name="Department Name")
+    c_code = models.IntegerField(verbose_name="Course Code")
+    c_name = models.CharField(max_length=200, verbose_name="Course Name")
+    c_seats = models.IntegerField(verbose_name="Number of Seats")
+    c_desc = models.TextField(blank=True, verbose_name="Course Description")
+
+    def __str__(self):
+        """
+        String representation of the object.
+        """
+        return self.c_name
+
+
+class Student(models.Model):
+    """
+    This model will store students' details.
+    Primary key will be the students' id.
+    """
+
+    class Meta:
+        db_table = 'students'
+        ordering = ['s_id']
+
+    s_id = models.IntegerField(primary_key=True, verbose_name="Student ID")
+    s_first_name = models.CharField(max_length=200, verbose_name="First Name")
+    s_last_name = models.CharField(max_length=200, verbose_name="Last Name")
+    s_pic = models.ImageField(upload_to='student_pics', blank=True, verbose_name="Student's pic")
+    course = models.ManyToManyField(Course, blank=True, verbose_name="Courses")
+
+    def __str__(self):
+        """
+        String representation of the student object.
+        """
+        return "{0} {1}".format(self.s_first_name, self.s_last_name)
 
 
