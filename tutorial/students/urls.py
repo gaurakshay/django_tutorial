b0461e89280085d1fb00bcdac3b4f59c087cf49f
gaurakshay <gaur.akshay@gmail.com>
2018-06-06 16:08:56 -0500

    finished list view, started details view

--- tutorial/students/urls.py
+++ tutorial/students/urls.py
@@ -3,4 +3,6 @@ from students import views
 
 urlpatterns = [
     path('', views.WelcomeView.as_view(), name='welcome'),
+    path('depts/', views.DeptListView.as_view(), name='dept_list'),
+    path('depts/<str:pk>/', views.DeptDetailView.as_view(), name='dept_details'),
 ]
--- tutorial/students/views.py
+++ tutorial/students/views.py
@@ -1,9 +1,37 @@
 from django.shortcuts import render
 
 # import the template view.
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+
+# import department model
+from students.models import Department
 
 
 class WelcomeView(TemplateView):
+    """
+    This is our welcome screen. Utilizes a
+    template view.
+    """
     template_name = 'welcome.html'
+
+
+class DeptListView(ListView):
+    """
+    This class handles the list view for
+    department model. Utilizes the list view
+    """
+    template_name = 'dept-list.html'
+    model = Department
+    context_object_name = 'depts'
+
+
+class DeptDetailView(DetailView):
+    """
+    This is the detail view of the class.
+    Uses the default detail view provided
+    by Django Framework.
+    """
+    template_name = 'dept-details.html'
+    model = Department
+    context_object_name = "dept"
 
